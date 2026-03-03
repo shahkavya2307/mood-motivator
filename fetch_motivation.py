25855dd9d56d5efcf5cfdf7f2f00cffeb444e1b1
shahkavya2307 <kavya.25bcs10125@sst.scaler.com>
2026-03-03 16:19:21 +0530

    Added files

---
+++ fetch_motivation.py
@@ -0,0 +1,57 @@
+import os 
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+gen_ai_key = os.getenv("GEMINI_API_KEY")
+
+def mood(user_mood,user_name):
+
+    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key={gen_ai_key}"
+
+    headers = {
+        'Content-Type' : "application/json" 
+    }
+
+
+    payload = {
+        "systemInstruction" : {
+            "parts" : [
+                {
+                "text" : "You are good motivator . On any specific you will help the user to be happy .Talk in a easy english and give some bhagwad gita sholak and its translation with rest to mood and help the person to come out of this delima"
+                }
+            ]
+        },
+        "contents": [
+            {
+            "parts" : [
+                            {
+                                "text": f"I am feeling{user_mood}.Give me a short , classy motivational boost. and my name is {user_name}"
+                            }
+                        ]
+            }
+        ],
+        "generationConfig" : {
+            "temperature" : 0.7,
+            "topP" : 0.95,
+        }
+
+    }
+
+
+    response = requests.post(url,headers=headers,json=payload)
+
+    if response.status_code == 200:
+        data = response.json()  
+
+        mood_message = data['candidates'][0]['content']['parts'][0]['text']
+
+        return mood_message
+    else:
+
+        return "Oops!"
+
+
+
+
---
+++ motivation_app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+from fetch_motivation import mood
+
+
+st.title("Classy MoodResidue Motivator")
+
+user_name = st.text_input("Your Name" , placeholder="Type here...." ) #label,placeholder
+
+
+user_mood = st.selectbox(
+    "Pls select how you feel right now",#Question 
+             [
+                 "😂 Happy Always",
+                 "😔 Sorry to hear",
+                 "😩 Stressed",
+                 "🥱 Tired and sleepy"
+              ])
+
+if st.button("Lets have a talk 😊"):
+
+    if user_name:#check username is there or not
+        mood_instructor = mood(user_mood,user_name)
+
+        st.write(mood_instructor)
+    
+    else:
+        st.warning("Please enter your name first!")
+
